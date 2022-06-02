5068f7beade8d6232d19e2131137a2ebf55f7e03
Daniel Coutino <dani2000coutino@gmail.com>
2022-06-02 18:58:35 -0500

    agragando html,css,js

---
+++ js/scrip.js
@@ -0,0 +1,15 @@
+function miMetodo(){
+	prompt("precionaste el boton");
+	console.log("");
+}
+function boton1(){
+	prompt("precionaste el boton1");
+	console.log("esto es una prueba");
+}
+function boton2(){
+	prompt("precionaste el boton2");
+	console.log("esto es una prueba");
+}
+
+
+	
